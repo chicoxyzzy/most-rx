d02794f38eb27140aa330ae5c461fab7e8702d8b
chico <chi187@gmail.com>
2016-06-12 02:15:28 +0300

    update dependencies

--- src/mostToRx.js
+++ src/mostToRx.js
@@ -1,7 +1,5 @@
 import Rx from 'rxjs/Rx';
 
 export function mostToRx(mostObservable) {
-  return Rx.Observable.create((observer) => {
-    return mostObservable.observe(value => observer.next(value));
-  });
+  return Rx.Observable.create(observer => mostObservable.observe(value => observer.next(value)));
 }
